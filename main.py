3dec90243127d08b7d02f1eadbb7ffec9ace51d9
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-27 16:45:49 +0200

    add: send to multiple users & fix return value

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import httpx # Yes, we could use requests but httpx supports async tasks and HTT
 from os import getenv
 from asyncio import run
 
+def get_users() -> list[str]:
+    return getenv('QDBotIDs').split(';')
+
 async def make_request_to_telegram(service_name: str) -> dict:
     message = f'⚠️ The service {service_name} results offline!'
-    url = f'https://api.telegram.org/bot{getenv("QDBotToken")}/sendMessage?chat_id={getenv("QDBotIDs")}&text={message}'
 
-    async with httpx.AsyncClient(http2=True) as client:
-        res = await client.post(url)
-        print(res)
-        return res
+    for chat_id in get_users():
+        url = f'https://api.telegram.org/bot{getenv("QDBotToken")}/sendMessage?chat_id={chat_id}&text={message}'
+
+        async with httpx.AsyncClient(http2=True) as client:
+            res = await client.post(url)
+            return res.json()
 
 
 if __name__ == '__main__':
