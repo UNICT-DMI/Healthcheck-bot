86dd4dac40cadf7e2e8113543d91132b987868a0
Valeri0p <24795760+Valeri0p@users.noreply.github.com>
2022-05-28 20:52:59 +0200

    first commit

--- src/main.py
+++ src/main.py
@@ -5,18 +5,27 @@ from os import getenv
 from asyncio import run
 
 def get_users() -> list[str]:
+    print(getenv('QDBotIDs'))
     return getenv('QDBotIDs').split(';')
 
+def check_ok(url: str) -> bool:
+    r = httpx.get(url)
+    return r.status_code == httpx.codes.OK
+
 async def make_request_to_telegram(service_name: str) -> dict:
-    message = f'⚠️ The service {service_name} results offline!'
+    #TODO: implement check with ping
+    if check_ok(service_name) == False:
+        print("async")
+        message = f'⚠️ The service {service_name} results offline!'
 
-    for chat_id in get_users():
-        url = f'https://api.telegram.org/bot{getenv("QDBotToken")}/sendMessage?chat_id={chat_id}&text={message}'
+        for chat_id in get_users():
+            print("id: " + chat_id)
+            url = f'https://api.telegram.org/bot{getenv("QDBotToken")}/sendMessage?chat_id={chat_id}&text={message}'
 
-        async with httpx.AsyncClient(http2=True) as client:
-            res = await client.post(url)
-            return res.json()
+            async with httpx.AsyncClient(http2=True) as client:
+                res = await client.post(url)
+                return res.json()
 
 
 if __name__ == '__main__':
-    run(make_request_to_telegram('test'))
+    run(make_request_to_telegram('http://duckduckgo.com/'))
