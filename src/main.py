c99a38026b18bcd7e7dca5c3b349645c526c20d9
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-29 10:57:54 +0200

    feat: introducing unit tests

--- src/main.py
+++ src/main.py
@@ -3,20 +3,61 @@
 import httpx # Yes, we could use requests but httpx supports async tasks and HTTP/2!
 from os import getenv
 from asyncio import run
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+
 
 def get_users() -> list[str]:
     return getenv('QDBotIDs').split(';')
 
-async def make_request_to_telegram(service_name: str) -> dict:
-    message = f'⚠️ The service {service_name} results offline!'
 
-    for chat_id in get_users():
-        url = f'https://api.telegram.org/bot{getenv("QDBotToken")}/sendMessage?chat_id={chat_id}&text={message}'
-
-        async with httpx.AsyncClient(http2=True) as client:
-            res = await client.post(url)
-            return res.json()
+def check_ok(url: str) -> bool:
+    r = httpx.get(url)
+    #there are various status codes, duckduckgo returns 301; is there a keyword only for errors?
+    #return r.status_code == httpx.codes.OK
+    return r.status_code < 400
 
 
-if __name__ == '__main__':
-    run(make_request_to_telegram('test'))
+def my_ping(host):
+    """
+    Returns True if host (str) responds to a ping request.
+    Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
+    """
+
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+
+    return subprocess.call(command) == 0
+
+
+async def make_request_to_telegram(service_name: str,hostname: str) -> dict:
+    
+    if check_ok(service_name) == False or my_ping(hostname) == False:
+        
+        message = f'⚠️ The service {service_name} results offline!'
+
+        #print("getusers: ")
+        #print(get_users())
+
+        for chat_id in get_users():
+            print("id: " + chat_id)
+            url = f'https://api.telegram.org/bot{getenv("QDBotToken")}/sendMessage?chat_id={chat_id}&text={message}'
+
+            async with httpx.AsyncClient(http2=True) as client:
+                res = await client.post(url)
+                return res.json()
+
+
+async def main() -> None:
+    await make_request_to_telegram('https://duckduckgo.com/')
+
+
+async def init() -> None:
+    if __name__ == '__main__':
+        await main() 
+
+
+run(init())
---
+++ tests/main_test.py
@@ -0,0 +1,51 @@
+from asyncio import Future
+import pytest
+from pytest_mock import MockerFixture
+from unittest.mock import AsyncMock
+import src.main as main
+
+
+tests = [
+    {
+        'func': main.get_users,
+        'expected_res': ['12345678'],
+        'arg': tuple(),
+        'mock_obj': [main],
+        'mock_func': ['getenv'],
+        'mock_ret': ['12345678'],
+        'is_async': False
+    },
+    {
+        'func': main.get_users,
+        'expected_res': ['12345678', '23456789'],
+        'arg': tuple(),
+        'mock_obj': [main],
+        'mock_func': ['getenv'],
+        'mock_ret': ['12345678;23456789'],
+        'is_async': False
+    }
+]
+
+@pytest.mark.parametrize('test', tests)
+async def test_generic(mocker: MockerFixture, test: dict) -> None:
+    spyed_objs = []
+    if test.get('mock_obj') is not None:
+        for index, obj in enumerate(test['mock_obj']):
+            mocker.patch.object(obj, test['mock_func'][index], return_value=test['mock_ret'][index])
+            spyed_objs.append(mocker.spy(obj, test['mock_func'][index]))
+
+    if test['is_async']:
+        res = await test['func'](*test['arg'])
+    else:
+        res = test['func'](*test['arg'])
+
+    assert res == test['expected_res']
+    for index, spy in enumerate(spyed_objs):
+        assert spy.spy_return == test['mock_ret'][index]
+
+async def test_init(mocker: MockerFixture) -> None:
+    mocker.patch.object(main, "__name__", "__main__")
+    mocker.patch.object(main, 'main', return_value=None)
+
+    assert await main.init() == None
+    
