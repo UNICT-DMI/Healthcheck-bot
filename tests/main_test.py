d41666e089642962e9d3393f4f06224e689be582
Domenico Blanco <domenicog.blanco@gmail.com>
2022-05-29 19:20:52 +0200

    improvement: remove 'is_async' when possible

--- tests/main_test.py
+++ tests/main_test.py
@@ -6,21 +6,11 @@ import src.main as main
 tests = [
     {
         'func': main.get_users,
-        'expected_res': ['12345678'],
-        'arg': tuple(),
-        'mock_obj': [main],
-        'mock_func': ['getenv'],
-        'mock_ret': ['12345678'],
-        'is_async': False
-    },
-    {
-        'func': main.get_users,
         'expected_res': ['12345678', '23456789'],
         'arg': tuple(),
         'mock_obj': [main],
         'mock_func': ['getenv'],
         'mock_ret': ['12345678;23456789'],
-        'is_async': False
     },
     {
         'func': main.check_ok,
@@ -38,19 +28,24 @@ tests = [
         'func': main.check_ping,
         'expected_res': True,
         'arg': ('example.org',),
-        'is_async': False
     },
     {
         'func': main.check_ping,
         'expected_res': False,
         'arg': ('wrongurlpage.com',),
-        'is_async': False
     }, 
     {
         'func': main.obtain_hostname,
         'expected_res': 'example.com',
         'arg': ('http://example.com',),
-        'is_async': False
+    },
+    {
+        'func': main.get_users,
+        'expected_res': ['12345678'],
+        'arg': tuple(),
+        'mock_obj': [main],
+        'mock_func': ['getenv'],
+        'mock_ret': ['12345678'],
     },
     {
         'func': main.make_request_to_telegram,
@@ -68,7 +63,6 @@ tests = [
         'mock_obj': [main, main, main],
         'mock_func': ['get_users', 'run', 'sleep'],
         'mock_ret': [['12345678'], {"ok":False,"error_code":429,"description":"Unauthorized"}, None],
-        'is_async': False
     },
     {
         'func': main.handle_communication,
@@ -77,7 +71,6 @@ tests = [
         'mock_obj': [main, main],
         'mock_func': ['get_users', 'run'],
         'mock_ret': [['12345678'], {"ok":True,"error_code":429,"description":"Unauthorized"}],
-        'is_async': False
     },
     {
         'func': main.handle_communication,
@@ -86,7 +79,6 @@ tests = [
         'mock_obj': [main],
         'mock_func': ['get_users'],
         'mock_ret': [[]],
-        'is_async': False
     },
     {
         'func': main.main,
@@ -95,7 +87,6 @@ tests = [
         'mock_obj': [main],
         'mock_func': ['handle_urls'],
         'mock_ret': [None],
-        'is_async': False
     }
 ]
 
@@ -107,10 +98,10 @@ async def test_generic(mocker: MockerFixture, test: dict) -> None:
             mocker.patch.object(obj, test['mock_func'][index], return_value=test['mock_ret'][index])
             spyed_objs.append(mocker.spy(obj, test['mock_func'][index]))
 
-    if test['is_async']:
-        res = await test['func'](*test['arg'])
-    else:
+    if test.get('is_async') is None:
         res = test['func'](*test['arg'])
+    else:
+        res = await test['func'](*test['arg'])        
 
     assert res == test['expected_res']
     for index, spy in enumerate(spyed_objs):
